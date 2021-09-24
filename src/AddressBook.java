6a145b9eaa615d161aeabcfbf6a7d751361a1dc2
Raul <rvhoyos@gmail.com>
2021-09-24 12:01:00 -0400

    updated AddressBook for lab 2.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,29 +1,30 @@
 import java.util.ArrayList;
 
 public class AddressBook {
-
-    BuddyInfo Buddy;
-    ArrayList addressBook;
+    private ArrayList addressBook;
 
     public AddressBook(){
 
-        addressBook = new ArrayList();
+        addressBook = new ArrayList<>();
     }
 
 
-    public void addBuddy(){
-    addressBook.add(Buddy);
+    public void addBuddy(BuddyInfo Buddy){
+    if(Buddy != null){
+        addressBook.add(Buddy);
+        }
     }
 
-    public void removeBuddy(){
-        addressBook.remove(Buddy);
+    public void removeBuddy(int index) {
+        if (index >= 0 && index < addressBook.size()) {
+            addressBook.remove(index);
+        }
     }
-
     public static void main(String[] args) {
         BuddyInfo Buddy = new BuddyInfo("Tom","Carleton",613);
-        AddressBook addressBook = new AddressBook(Buddy,);
-        addressBook.addBuddy();
-        addressBook.removeBuddy();
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(Buddy);
+        addressBook.removeBuddy(0);
 
         System.out.println("Address Book");
 
---
+++ src/BuddyInfo.java
@@ -0,0 +1,42 @@
+public class   BuddyInfo {
+    private String name;
+    private String address;
+    private int phoneNumber;
+
+    public BuddyInfo(String name, String address,int phoneNumber){
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+   /* public static void main(String[] args) {
+        System.out.println("Hello,");
+      //  BuddyInfo firstBuddy = new BuddyInfo("Raul H", "Canada", 226-777-7777);
+      //  System.out.println(firstBuddy.getName());
+
+    }*/
+}
